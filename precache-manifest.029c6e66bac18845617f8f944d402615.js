011558303ffeedd3351fbbf5e4b48b2c135807c5
itsrealoj <stephenojwang1040@gmail.com>
2020-04-23 15:15:06 +0300

    Updates

---
+++ precache-manifest.029c6e66bac18845617f8f944d402615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4f9a973b1534a4489f1f13a133de65",
+    "url": "/hacker-news-reader/static/media/logol.bd4f9a97.png"
+  },
+  {
+    "revision": "4677c5005ccbece03981",
+    "url": "/hacker-news-reader/static/js/runtime~main.4677c500.js"
+  },
+  {
+    "revision": "3d90541652f8579aebad",
+    "url": "/hacker-news-reader/static/js/main.3d905416.chunk.js"
+  },
+  {
+    "revision": "4c5403fb5d928e22cdec",
+    "url": "/hacker-news-reader/static/js/1.4c5403fb.chunk.js"
+  },
+  {
+    "revision": "9506220bd10c7daa0463c3cf7f362903",
+    "url": "/hacker-news-reader/index.html"
+  }
+];
