59fcf4721950396e31108b3df2f401fbf331d222
itsrealoj <stephenojwang1040@gmail.com>
2020-04-23 14:43:18 +0300

    Updates

---
+++ precache-manifest.ea39fc428f3c400a23e609798b5f03be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd4f9a973b1534a4489f1f13a133de65",
+    "url": "/static/media/logol.bd4f9a97.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e46268e2c24483af225",
+    "url": "/static/js/main.7e46268e.chunk.js"
+  },
+  {
+    "revision": "c92dd985917f6de46c8f",
+    "url": "/static/js/1.c92dd985.chunk.js"
+  },
+  {
+    "revision": "2b4a152d608a921bfabf4f802b527bd8",
+    "url": "/index.html"
+  }
+];
